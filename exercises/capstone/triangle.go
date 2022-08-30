8069d7cc4f5c5d09327e3d6e14c17e23f40c092e
Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>
2022-08-30 07:16:43 -0400

    Create triangle.go

---
+++ exercises/capstone/triangle.go
@@ -0,0 +1,32 @@
+func minimumTotal(triangle [][]int) int {
+	cache := map[[2]int]int{}
+	return findMinPathSum(triangle, 0, 0, cache) // pass row, column and cache
+}
+
+func findMinPathSum(triangle [][]int, row, column int, cache map[[2]int]int) int {
+	if row == len(triangle)-1 { // Base case when at last row
+		return triangle[row][column]
+	}
+
+	if val, ok := cache[[2]int{row, column}]; ok { // if exists in cache, return value
+		return val
+	}
+
+	currValue := triangle[row][column]
+	firstSum := findMinPathSum(triangle, row+1, column, cache)    //recurse with same column
+	secondSum := findMinPathSum(triangle, row+1, column+1, cache) // recurse with column + 1
+
+	var sumToAdd int
+
+	if firstSum > secondSum { // add current value to minimum sum of returned recursive functions and return
+		sumToAdd = currValue + secondSum
+	} else {
+		sumToAdd = currValue + firstSum
+	}
+	fmt.Println(sumToAdd)
+
+	cache[[2]int{row, column}] = sumToAdd
+	return cache[[2]int{row, column}]
+}
+
+
